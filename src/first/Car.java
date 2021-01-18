8aa559f301c3f8c6f1383f60dbf0a13272471962
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-18 15:49:20 +0900

    객체의 속성 (필드)

--- src/first/Car.java
+++ src/first/Car.java
@@ -1,5 +1,7 @@
 package first;
 
 public class Car {
-	
+	//학업 필드명
+	String name;
+	int number;
 }
--- src/first/CarExam.java
+++ src/first/CarExam.java
@@ -7,6 +7,15 @@ public class CarExam {
 		//c1, c2는 메모리에 올라긴 new Car()의 위치 값이 저장되어있음.
 		Car c1 = new Car();
 		Car c2 = new Car();
+		
+		c1.name = "소방차";
+		c1.number = 119;
+		
+		c2.name = "경찰차";
+		c2.number = 112;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
 	}
 
 }
