6b83fab1642606736a21dac85aa7d1aa957c2185
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-18 20:29:34 +0900

    접근제한자

---
+++ src/com/eightcruz/javastudy/AccessObjExam.java
@@ -0,0 +1,16 @@
+package com.eightcruz.javastudy;
+
+import first.AccessObj;
+
+public class AccessObjExam extends AccessObj{
+
+	public static void main(String[] args) {
+//		AccessObj obj = new AccessObj();
+		AccessObjExam obj = new AccessObjExam();
+		System.out.println(obj.p);
+		System.out.println(obj.p2);
+//		System.out.println(obj.k);
+//		System.out.println(obj.i);
+	}
+
+}
---
+++ src/first/AccessObj.java
@@ -0,0 +1,11 @@
+package first;
+
+public class AccessObj {
+	public int p = 3;
+	//같은 패키지
+	//다른 패키지라도 상속된경우
+	protected int p2 = 4;
+	//같은 패키지에서만
+	int k = 2;
+	private int i = 1;
+}
