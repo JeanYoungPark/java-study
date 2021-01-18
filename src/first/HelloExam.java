840cc3f04dd26c4489da8a285a420c4b7f75a9d2
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-18 18:11:38 +0900

    패키지, 임포트

---
+++ src/first/HelloExam.java
@@ -0,0 +1,13 @@
+package first;
+
+//ctrl+shift+o 패키지 임포트 단축키
+//다른 패키지의 클래스 사용을 위해 필요
+import com.eightcruz.javastudy.*;
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+	}
+
+}
