e1d6e7193d439b84d1970e6b68bbc16577d17349
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-19 21:48:37 +0900

    사용자 정의 Exception

---
+++ src/first/BizExam.java
@@ -0,0 +1,15 @@
+package first;
+
+public class BizExam {
+
+	public static void main(String[] args) {
+		BizService biz = new BizService();
+		biz.bizMethod(5);
+		try {
+			biz.bizMethod(-2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
