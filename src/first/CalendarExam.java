479a89da0bd3743f07696842f1bb1926daf20285
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-21 21:22:00 +0900

    Calendar

---
+++ src/first/CalendarExam.java
@@ -0,0 +1,22 @@
+package first;
+
+import java.util.Calendar;
+
+public class CalendarExam {
+
+	public static void main(String[] args) {
+		//calendar이 추상 클래스 
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal.get(Calendar.YEAR));
+		System.out.println(cal.get(Calendar.MONTH)+1);
+		System.out.println(cal.get(Calendar.DATE));
+		
+		System.out.println(cal.get(Calendar.HOUR));
+		
+		cal.add(Calendar.HOUR, 5);
+		
+		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+		
+	}
+
+}
