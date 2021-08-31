caf24944d58685b3c5d14d8a58e1657143c544d9
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-08-31 22:08:49 +0900

    쓰레드

---
+++ src/study/ThreadEx.java
@@ -0,0 +1,27 @@
+package study;
+
+import javax.swing.*;
+
+public class ThreadEx {
+    public static void main(String[] args) {
+        ThreadEx_1 th1 = new ThreadEx_1();
+        th1.start();
+
+        String input = JOptionPane.showInputDialog("아무 값이나 입력하세요");
+        System.out.println("입력하신 값은 " + input + "입니다.");
+
+    }
+
+    static class ThreadEx_1 extends Thread {
+        public void run() {
+            for (int i = 10; i > 0; i--) {
+                System.out.println(i);
+                try {
+                    sleep(1000);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+}
